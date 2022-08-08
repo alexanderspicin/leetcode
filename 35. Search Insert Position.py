184bed7db3d8a7750936ed3f825bb8fb89ee367a
alexanderspicin <spicin.2011@mail.ru>
2022-08-08 14:39:57 +0300

    add submissions

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,11 +1,12 @@
 """
-Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
 """
 
+
 class Solution(object):
-    def search(self, nums, target):
+    def searchInsert(self, nums, target):
         low = 0
         high = len(nums) - 1
         while low <= high:
@@ -16,4 +17,4 @@ class Solution(object):
                 high = middle - 1
             else:
                 low = middle + 1
-        return -1
+        return low
