0c86b65d74e9c2e8ad690d949c894aa5785f364d
alexanderspicin <spicin.2011@mail.ru>
2022-08-08 14:05:01 +0300

    fix bag

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def search(self, nums, target):
         low = 0
-        high = len(nums)
+        high = len(nums) - 1
         while low <= high:
             middle = int((low + high) / 2)
             if target == nums[middle]:
                 return middle
             elif target < nums[middle]:
-                high = middle
+                high = middle - 1
             else:
-                low = middle
-
-
+                low = middle + 1
+        return -1
